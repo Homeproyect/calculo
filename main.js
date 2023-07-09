d3a512e1db5e8a4fd4aa367824a3cbdfc5328775
Homeproyect <113480886+Homeproyect@users.noreply.github.com>
2023-07-08 20:58:38 -0600

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,13 @@ function go(){
 var dosis =document.getElementById("dosis").value; 
 var peso =document.getElementById("peso").value; 
 var cantidad =document.getElementById("cantidad").value; 
+var porcentaje =document.getElementById("porcentaje").value; 
+
+
 
 var resultado=dosis*peso*cantidad;
-document.getElementById("resultado").value=(resultado);
-document.getElementById("resultado1").value=(resultado/1000000);
+
+var resultado2=(resultado*100)/porcentaje;
+document.getElementById("resultado").value=(resultado2);
+document.getElementById("resultado1").value=(resultado2/1000000);
 }
